0634b437c0a4efa31bfdbaa0dea250d1183d1122
ktanaka101 <kentanaka101@gmail.com>
2019-09-17 00:14:32 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# priority_queue
+
+TODO: Write a description here
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     priority_queue:
+       github: ktanaka101/priority_queue
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "priority_queue"
+```
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/ktanaka101/priority_queue/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [ktanaka101](https://github.com/ktanaka101) - creator and maintainer
---
+++ src/priority_queue.cr
@@ -0,0 +1,74 @@
+module PriorityQueue(T)
+  VERSION = "0.1.0"
+
+  def self.build : PriorityQueue(T)
+    PriorityQueue(T).new
+  end
+
+  class PriorityQueue(T)
+    include Enumerable(T)
+
+    def initialize
+      @a = Array(T).new
+    end
+
+    def each
+      @a.each do |o|
+        yield o
+      end
+    end
+
+    delegate size, to: @a
+
+    def push(key : T)
+      @a << T::MIN
+      increase_key(@a.size - 1, key)
+    end
+
+    def <<(key : T)
+      push(key)
+    end
+
+    def pop : T
+      return T::MIN if @a.empty?
+
+      maxv = @a[0]
+      @a.size > 1 ? (@a[0] = @a.pop) : @a.pop
+      max_heapify(0)
+
+      maxv
+    end
+
+    private def increase_key(i, key)
+      return if key < @a[i]
+
+      @a[i] = key
+
+      c_idx = i
+      p_idx = (c_idx - 1) / 2
+      while c_idx > 0 && @a[p_idx] < @a[c_idx]
+        @a.swap(p_idx, c_idx)
+        c_idx = (c_idx - 1) / 2
+        p_idx = (c_idx - 1) / 2
+      end
+    end
+
+    private def max_heapify(i : Int)
+      l = i * 2 + 1
+      r = l + 1
+
+      largest =
+        if l < @a.size && @a[l] > @a[i]
+          l
+        else
+          i
+        end
+      largest = r if r < @a.size && @a[r] > @a[largest]
+
+      unless largest == i
+        @a.swap(i, largest)
+        max_heapify(largest)
+      end
+    end
+  end
+end
